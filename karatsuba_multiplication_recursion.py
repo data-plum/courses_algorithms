8e569b1f07bdabdefc04f4ab59261f0ac48905d8
data-plum <ivanka.feleschuk@gmail.com>
2015-06-20 18:02:32 +0300

    Almost rigth 2

--- karatsuba_multiplication_recursion.py
+++ karatsuba_multiplication_recursion.py
@@ -1,18 +1,3 @@
-def karatsuba_multiplication(x, y):
-	x_size = len((str(x)))
-	y_size = len((str(y)))
-	
-	x1 = int((str(x))[:(x_size / 2)])
-	x2 = int((str(x))[(x_size / 2):])
-	y1 = int((str(y))[:(y_size / 2)])
-	y2 = int((str(y))[(y_size / 2):])
-
-	n = x_size if x_size > y_size else y_size
-
-	mult = ((10**n)*x1*y1) + ((10**(n / 2))*((x1+x2)*(y1+y2) - x1*y1 - x2*y2)) + x2*y2
-	return mult
-
-
 def multiplication(num1, num2, counter): 
 	if len(str(num1)) == 1 and len(str(num2)) == 1:
 		return (long(num1) * long(num2), counter)
@@ -45,22 +30,7 @@ def multiplication(num1, num2, counter):
 
 
 counter = 0
-# a = 1685287499328328297814655639278583667919355849391453456921116729
-# b = 7114192848577754587969744626558571536728983167954552999895348492	
-# right_res = 11989460275519080564894036768322865785999566885539505969749975204962718118914971586072960191064507745920086993438529097266122668
-a = 71611955325935479159397713213124
-b = 93567726499788166681348352945366
-right_res = 6700567850052179472481148730882040129649508491917840721086183384
-
+a = 1685287499328328297814655639278583667919355849391453456921116729
+b = 7114192848577754587969744626558571536728983167954552999895348492
 
 res = multiplication(a, b, counter)
-hues = karatsuba_multiplication(a, b)
-
-if hues == right_res:
-	print True
-
-if res[0] == right_res:
-	print  True
-
-print res[1]
-			
